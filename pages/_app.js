4de9d40b8d90e7bc2684a5fd9e24e2605758f371
Gordon <gordon@hey.com>
2022-07-01 15:51:45 -0700

    added assembly favicons

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '@styles/globals.css'
+import '@styles/globals.css';
+import Head from 'next/head';
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+     <Head>
+      <link rel="icon" type="image/png" href="/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+      <link rel="manifest" href="/site.webmanifest"/>
+    </Head>
+
+  <Component {...pageProps} />
+
+  </>
+  );
 }
 
-export default Application
+
+export default Application;
